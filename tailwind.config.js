6c63a26982e74cefff60215e07c439cc40108779
Winston Nji <winston00russel@gmail.com>
2025-01-17 13:01:07 +0200

    Add Tailwind CSS setup and completed html and styling for nav element

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        'navColor' : '#d5d5d5',
+        'btnColor' : '#9e8a4b'
+      }
+    },
+  },
+  plugins: [],
+}
+
